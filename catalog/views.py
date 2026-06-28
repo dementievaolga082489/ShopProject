ffe48c3184d73aa165b448b79bb7f64d5e0c9146
Olga Dementieva <08289@mail.ru>
2026-06-28 12:58:03 +0300

    Добавила изменения в views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product, Category, Contact
 
 
 def home(request):
     # Получаем последние 5 созданных продуктов
-    latest_products = Product.objects.all().order_by('-created_at')[:5]
+    latest_products = Product.objects.all().order_by("-created_at")[:5]
 
     # Вывод в консоль (для отладки)
     for i, product in enumerate(latest_products, 1):
@@ -14,11 +14,7 @@ def home(request):
         print(f"Категория: {product.category.name}")
         print(f"Создан: {product.created_at.strftime('%d.%m.%Y %H:%M')}")
 
-
-
-    return render(request, "home.html", {
-            'latest_products': latest_products
-        })
+    return render(request, "home.html", {"latest_products": latest_products})
 
 
 def contacts(request):
@@ -29,9 +25,9 @@ def contacts(request):
     categories = Category.objects.all()
 
     context = {
-        'contacts': contacts,
-        'categories': categories,
-        'title': 'Контакты',
+        "contacts": contacts,
+        "categories": categories,
+        "title": "Контакты",
     }
     return render(request, "contacts.html", context)
 
@@ -45,3 +41,15 @@ def contact(request):
             f"Спасибо, {name}, контактный телефон - {phone}! Ваше сообщение получено."
         )
     return render(request, "contacts.html")
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context)

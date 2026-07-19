5d1752e561bc64cdf97bea3a3d62c7338faf617f
Olga Dementieva <08289@mail.ru>
2026-07-19 15:24:35 +0300

    добавление изменений в urls.py blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,22 +1,29 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogPostCreateView, BlogPostDetailView, BlogPostListView, BlogPostDeleteView, BlogPostUpdateView
+from blog.views import (
+    BlogPostCreateView,
+    BlogPostDetailView,
+    BlogPostListView,
+    BlogPostDeleteView,
+    BlogPostUpdateView,
+)
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     # Список всех записей
-    path('', BlogPostListView.as_view(), name='post_list'),
-
+    path("", BlogPostListView.as_view(), name="post_list"),
     # Детальная страница записи
-    path('post/<int:pk>/', BlogPostDetailView.as_view(), name='post_detail'),
-
+    path("post/<int:pk>/", BlogPostDetailView.as_view(), name="post_detail"),
     # Создание записи
-    path('post/create/', BlogPostCreateView.as_view(), name='post_create'),
-
+    path("post/create/", BlogPostCreateView.as_view(), name="post_create"),
     # Редактирование записи
-    path('post/<int:pk>/update/', BlogPostUpdateView.as_view(), name='post_update'),
-
+    path("post/<int:pk>/update/", BlogPostUpdateView.as_view(), name="post_update"),
     # Удаление записи
-    path('post/<int:pk>/delete/', BlogPostDeleteView.as_view(), name='post_confirm_delete'),]
+    path(
+        "post/<int:pk>/delete/",
+        BlogPostDeleteView.as_view(),
+        name="post_confirm_delete",
+    ),
+]

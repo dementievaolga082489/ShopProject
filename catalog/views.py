3105f37c8a9fbcfc5295ca89659cf227a17d630a
Olga Dementieva <08289@mail.ru>
2026-07-19 15:26:06 +0300

    добавление изменений в views.py catalog

--- catalog/views.py
+++ catalog/views.py
@@ -3,10 +3,18 @@ from idlelib.textview import ViewWindow
 
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, View
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    View,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
+from catalog.forms import ProductForm
 from catalog.models import Product, Category, Contact
-
 
 
 def home(request):
@@ -34,39 +42,40 @@ class ContactView(View):
         categories = Category.objects.all()
 
         context = {
-            'contacts': contacts,
-            'categories': categories,
-            'title': 'Контакты',
+            "contacts": contacts,
+            "categories": categories,
+            "title": "Контакты",
         }
 
-        return render(request, 'catalog/contacts.html', context)
+        return render(request, "catalog/contacts.html", context)
 
-
-#def contacts(request):
+    # def contacts(request):
     # Получаем контактные данные из базы
-#    contacts = Contact.objects.first()
+    #    contacts = Contact.objects.first()
 
     # Получаем все категории для навигации
-#    categories = Category.objects.all()
+    #    categories = Category.objects.all()
 
-#    context = {
-#        "contacts": contacts,
-#        "categories": categories,
-#        "title": "Контакты",
-#    }
-#    return render(request, "contacts.html", context)
+    #    context = {
+    #        "contacts": contacts,
+    #        "categories": categories,
+    #        "title": "Контакты",
+    #    }
+    #    return render(request, "contacts.html", context)
 
     def post(self, request, *args, **kwargs):
-    # Обработка отправки формы обратной связи
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+        # Обработка отправки формы обратной связи
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
 
-    # Здесь можно отправить письмо или сохранить в БД
-        messages.success(request, 'Ваше сообщение отправлено!')
+        # Здесь можно отправить письмо или сохранить в БД
+        messages.success(request, "Ваше сообщение отправлено!")
 
         return self.get(request, *args, **kwargs)
-#def contact(request):
+
+
+# def contact(request):
 #    if request.method == "POST":
 #        name = request.POST.get("name")
 #        phone = request.POST.get("phone")
@@ -76,19 +85,41 @@ class ContactView(View):
 #        )
 #    return render(request, "contacts.html")
 
+
 class ProductListView(ListView):
     model = Product
 
 
-#def products_list(request):
+# def products_list(request):
 #    products = Product.objects.all()
 #    context = {"products": products}
 #    return render(request, "product_list.html", context)
 
+
 class ProductDetailView(DetailView):
     model = Product
 
-#def product_detail(request, pk):
+
+# def product_detail(request, pk):
 #    product = get_object_or_404(Product, pk=pk)
 #    context = {"product": product}
 #    return render(request, "product_detail.html", context)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:products_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "catalog/product_form.html"
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:products_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:products_list")

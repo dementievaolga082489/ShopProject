cee24f08dc59602c0fe40ca733da4ed8fcf512af
Olga Dementieva <08289@mail.ru>
2026-07-25 13:37:38 +0300

    добавление изменений в urls config

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.global_settings import MEDIA_ROOT
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
@@ -8,4 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
     path("blog/", include("blog.urls", namespace="blog")),
+    path("users/", include("users.urls", namespace="users")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

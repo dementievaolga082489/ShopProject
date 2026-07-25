422439187b38cab7b28e79b546aea251284f0b07
Olga Dementieva <08289@mail.ru>
2026-07-25 13:38:15 +0300

    добавление urls users

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import UserCreateView, custom_logout, email_verification
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", custom_logout, name="logout"),
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
+]
